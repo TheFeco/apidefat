fe002ffe4c324f123a18b246c4c64845f84ea5ba
theFeco <francisco.fcospinoza@gmail.com>
2022-03-29 14:30:59 -0700

    [ADD] logout

--- logout.php
+++ logout.php
@@ -4,7 +4,6 @@ session_start();
 unset($_SESSION["s_usuario"]);
 unset($_SESSION["s_id]);
 session_destroy();
-unset($_COOKIE['PHPSESSID']);
 $data=[];
 print json_encode($data);
 header("Location:../index.php");
