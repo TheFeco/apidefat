dcc9c5f8a262359ccca95039090eb6e320f03c5d
theFeco <francisco.fcospinoza@gmail.com>
2022-03-29 14:32:49 -0700

    [ADD] logout

--- logout.php
+++ logout.php
@@ -2,7 +2,7 @@
 header('Access-Control-Allow-Origin: *');
 session_start();
 unset($_SESSION["s_usuario"]);
-unset($_SESSION["s_id]);
+unset($_SESSION["s_id"]);
 session_destroy();
 $data=[];
 print json_encode($data);
