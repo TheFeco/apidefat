58d7dee1a461e8eb7e8bb8b4d07359857805438d
fespinoza <f.espinoza@whipple.mx>
2023-02-15 21:12:52 -0700

    [Modificaciones] Agregando un token al login

--- db/conexion.php
+++ db/conexion.php
@@ -1,20 +1,37 @@
 <?php 
-class Conexion{	  
-    public static function Conectar() {        
-        define('servidor', 'localhost');
-        define('nombre_bd', 'defat');
-        define('usuario', 'root');
-        define('password', 't3cnolog14');					        
-        // define('password', '');					        
+class Conexion{
+    private $server;
+    private $user;
+    private $password;
+    private $database;
+    private $port;
+
+    public function Conectar() {        
+        
+        $listadatos = $this->datosConexion();
+        foreach ($listadatos as $key => $value) {
+            $this->server = $value['server'];
+            $this->user = $value['user'];
+            $this->password = $value['password'];
+            $this->database = $value['database'];
+            $this->port = $value['port'];
+        }				        
         $opciones = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8');			
         try{
-            $conexion = new PDO("mysql:host=".servidor."; dbname=".nombre_bd, usuario, password, $opciones);			
+            $conexion = new PDO("mysql:host=". $this->server."; dbname=".$this->database, $this->user, $this->password, $opciones);			
             return $conexion;
         }catch (Exception $e){
             die("El error de Conexión es: ". $e->getMessage());
         }
     }
+
+    private function datosConexion(){
+        $direccion = dirname(__FILE__);
+        $jsondata = file_get_contents($direccion . "/". "config");
+        return json_decode($jsondata, true);
+    }
 }
+
 
 function metodoGet($query){
     $objeto = new Conexion();
--- login.php
+++ login.php
@@ -1,10 +1,10 @@
 <?php
-//session_start();
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+header('Access-Control-Allow-Headers: Content-Type');
 
 include_once 'db/conexion.php';
-//include 'db/db.php';
 
-header('Access-Control-Allow-Origin: *');
 $objeto = new Conexion();
 $conexion = $objeto->Conectar();
 
@@ -14,14 +14,29 @@ $password = (isset($_POST['password'])) ? $_POST['password'] : '';
 
 $pass = md5($password); //encripto la clave enviada por el usuario para compararla con la clava encriptada y almacenada en la BD
 
-$consulta = "SELECT id, id_rol, id_nivel, usuario, sn_actualizar FROM usuarios WHERE usuario='$usuario' AND password='$pass' ";
-
+$consulta = "SELECT id, id_rol, id_nivel, usuario, Estado FROM usuarios WHERE usuario='$usuario' AND password='$pass' ";
+// echo $consulta;
 $resultado = $conexion->prepare($consulta);
 $resultado->execute();
+// if ($resultado->execute()) {
+//     // La consulta se ejecutó correctamente, se puede continuar con el procesamiento
+//     echo $resultado->rowCount() ;
+//   } else {
+//     // Ocurrió un error en la consulta, se puede obtener información sobre el error
+//     $errorInfo = $conexion->errorInfo();
+//     // El mensaje de error se encuentra en la posición 2 del arreglo errorInfo
+//     $mensajeError = $errorInfo[2];
+//     // Puedes imprimir o enviar este mensaje de error al cliente para que lo vea
+//     echo "Ocurrió un error al ejecutar la consulta: " . $mensajeError;
+//     // También puedes hacer alguna otra acción para manejar el error, como registrar en un archivo de logs, enviar un correo electrónico al administrador, etc.
+//   }
 
 //$resultado=metodoGet($consulta); 
 if($resultado->rowCount() >= 1){
     $data = $resultado->fetch(PDO::FETCH_ASSOC);
+     // Llamada a la función insertarToken para generar y almacenar el token en la base de datos
+     $token = insertarToken($data['id']);
+     $data['token'] = $token;
     $_SESSION["s_usuario"] = $usuario;
     $_SESSION["s_id"] = $data['id'];
 }else{
@@ -32,6 +47,22 @@ if($resultado->rowCount() >= 1){
 
 print json_encode($data);
 $conexion=null;
+
+function insertarToken($idUsuario) {
+    $val = true; // valor true
+    $token = bin2hex(openssl_random_pseudo_bytes(16, $val)); // genera un token aleatorio
+    $fecha = date("Y-m-d H:i"); // fecha actual
+    $estado = "Activo"; // estado activo
+
+    $objeto = new Conexion();
+    $conexion = $objeto->Conectar();
+    
+    $consulta = "INSERT INTO usuarios_token (UsuarioId, Token, Estado, Fecha) VALUES ('$idUsuario', '$token', '$estado', '$fecha')";
+    $resultado = $conexion->prepare($consulta);
+    $resultado->execute();
+  
+    return $token;
+  }
 
 //usuarios de pruebaen la base de datos
 //usuario:admin pass:12345
