a2be1ffd00d25071213749a2a091e39e2c04e5d8
fespinoza <f.espinoza@whipple.mx>
2023-02-27 11:32:39 -0700

    [ver problema en el servidor]

--- usuarios.php
+++ usuarios.php
@@ -3,6 +3,10 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
 header('Access-Control-Allow-Headers: Content-Type');
 
+echo "ptos";
+
+die();
+
 include_once "clases/usuarios.class.php";
 include_once 'clases/respuestas.class.php';
 
