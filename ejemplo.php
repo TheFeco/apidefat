7c85f00ebf8c528b768a355bb93598db9b1da584
theFeco <francsico.fcospinoza@gmail.com>
2023-03-08 13:54:14 -0700

    chequeo de cors

--- ejemplo.php
+++ ejemplo.php
@@ -1,16 +1,3 @@
 <?php
-require_once __DIR__ . '/vendor/autoload.php';
-
-// Crear objeto mpdf
-$mpdf = new \Mpdf\Mpdf();
-
-// Generar el contenido del PDF
-$mpdf->WriteHTML('<h1>Hola, mundo!</h1>');
-
-// Verificar si hay errores
-if ($mpdf->getError()) {
-    echo 'Error al crear el PDF: ' . $mpdf->getError();
-}
-
-// Descargar el archivo PDF
-$mpdf->Output();
+phpinfo();
+?>
