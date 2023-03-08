381ec7fd028afe9d3cfc6339c02139c2d421367a
theFeco <francsico.fcospinoza@gmail.com>
2023-03-08 12:01:04 -0700

    chequeo de cors

--- excel.php
+++ excel.php
@@ -49,9 +49,10 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     }
         $temp_file = 'files/tmp/ExportarTodo.csv';
         var_dump($temp_file);
-        die();
+        
          $output= outputCsv($data, $temp_file);
          var_dump($output);
+         die();
         
         exit;
 }
