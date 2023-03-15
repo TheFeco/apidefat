819009f548dabface505742f935990c0a9306c07
theFeco <francsico.fcospinoza@gmail.com>
2023-03-15 11:03:49 -0700

    Update Formatos

--- exportExcel.php
+++ exportExcel.php
@@ -30,7 +30,7 @@ if($_POST['METHOD']=='POST'){
          WHEN turno = 4 THEN 'Discontinuo'
          WHEN turno = 5 THEN 'Continuo'
     END AS turno,
-    ,c.nombre AS ciclo, m.nombre AS municipio, f.nombre AS funcion, dp.nombre AS deporte, r.nombre AS rama, cat.nombre AS categoria, peso.nombre AS peso, pruebas.nombre AS prueba,
+    c.nombre AS ciclo, m.nombre AS municipio, f.nombre AS funcion, dp.nombre AS deporte, r.nombre AS rama, cat.nombre AS categoria, peso.nombre AS peso, pruebas.nombre AS prueba,
     CONCAT(CASE WHEN d.curp_pdf <> '' THEN '$baseURL' ELSE '' END, d.curp_pdf) AS curp_pdf,
     CONCAT(CASE WHEN d.cert_medico <> '' THEN '$baseURL' ELSE '' END, d.cert_medico) AS cert_medico,
     CONCAT(CASE WHEN d.carta_responsiva <> '' THEN '$baseURL' ELSE '' END, d.carta_responsiva) AS carta_responsiva,
