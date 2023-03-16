baf9e64b4524acc93e58095e0992f8387d132d63
theFeco <francsico.fcospinoza@gmail.com>
2023-03-16 16:43:04 -0700

    agregando usuario a Excel

--- generarExcel.php
+++ generarExcel.php
@@ -41,7 +41,7 @@ if($_POST['METHOD']=='POST'){
     LEFT JOIN categorias AS cat ON ( d.id_categoria = cat.id)
     LEFT JOIN peso ON (d.id_peso = peso.id)
     LEFT JOIN pruebas ON (d.id_prueba = pruebas.id)
-    INNER JOIN usuarios AS user (d.usuarios = user.id)
+    INNER JOIN usuarios AS user (d.id_usuairo = user.id)
     WHERE d.id_usuairo = '$id_usuario'
     AND d.id_ciclo = '$id_ciclo'
     AND d.id_funcion = '$id_funcion' 
